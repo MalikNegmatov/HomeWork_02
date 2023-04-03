7683ec6c6e8d97f1af0cb3ef510a5d0bec267d95
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-04 02:09:33 +0300

    Добавили разделители с номером задач при выводе результатов в консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-// ======= Begin Task 1 =========
-        var dog = 8.0;  // Вводим новую переменную из кольца действительных чисел dog.
-                        // Тип переменной dog задан присвоением значения 8.0.
+        var taskCount = 0;
+        var taskScreen = "Задача №";
+        // ======= Begin Task 1 =========
+        taskCount++;
+        System.out.println(taskScreen + taskCount + ".1");
 
+        var dog = 8.0;  // Вводим новую переменную присвоением значения 8.0;
         System.out.println("Our dog weighs " + dog + " kg."); // Выводим в консоли значение переменной dog.
         // Чтобы было не так скучно предположим, что в переменной хранится вес собаки.
         // Наша собака весит "dog" килограмм .
 
-        var cat = 3.6;  // Вводим новую переменную cat c присвоением значения 3.6;
-
+        var cat = 3.6;  // Вводим новую переменную cat присвоением значения 3.6;
         System.out.println("Our cat weighs " + cat + " kg."); // Выводим в консоли значение переменной cat.
         // В том же формате: Наш кот весит "cat" килограмм.
 
         var paper = 7639;  // Вводим новую переменную c присвоением значения 7639;
-
         System.out.println("There is " + paper + " symbols in my paper"); // Выводим в консоли значение переменной paper.
         // В моей статье содержится "paper" символов.
 
@@ -26,8 +27,10 @@ public class Main {
         String coloredCat = (char)27 + "[34m" + cat + (char)27 + "[0m"; // То же самое для cat
         String coloredPaper = (char)27 + "[34m" + paper + (char)27 + "[0m"; // То же самое для paper
 
+        System.out.println("\n" + taskScreen + taskCount + ".2");
+
         //Вывод значений переменных подсвеченных синим цветом
-        System.out.println("\n" + "Our dog weighs " + coloredDog + " kg.");
+        System.out.println("Our dog weighs " + coloredDog + " kg.");
         System.out.println("Our cat weighs " + coloredCat + " kg.");
         System.out.println("There is " + coloredPaper + " symbols in my paper");
 // ======= End Task 1 =========
@@ -37,32 +40,41 @@ public class Main {
         var c2 = (char)27 + "[31m";
 
 // ======= Begin Task 2 =======
+        taskCount++;
+        System.out.println("\n" + taskScreen + taskCount);
+
         // Прибавим к каждой переменной 4
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
 
         // выведем в консоль значения переменных
-        System.out.println("\n" + "Our dog weighs " + c1 + dog + c0 + " kg.");
+        System.out.println("Our dog weighs " + c1 + dog + c0 + " kg.");
         System.out.println("Our dog weighs " + c1 + cat + c0 + " kg.");
         System.out.println("There is " + c1 + paper + c0 + " symbols in my paper");
-        // ======= End Task 2 =========
+// ======= End Task 2 =========
 
 // ======= Begin Task 3 =======
+        taskCount++;
+        System.out.println("\n" + taskScreen + taskCount);
+
         // Вносим изменения в каждую переменную
         dog -= 3.5;
         cat -= 1.6;
         paper -= 7639; // I'm'sorry, но так привычнее
 
         // выведем в консоль значения переменных
-        System.out.println("\n" + "Our dog weighs " + c1 + dog + c0 + " kg.");
+        System.out.println("Our dog weighs " + c1 + dog + c0 + " kg.");
         System.out.println("Our dog weighs " + c1 + cat + c0 + " kg.");
         System.out.println("There is " + c1 + paper + c0 + " symbols in my paper");
-        // ======= End Task 3 =========
+// ======= End Task 3 =========
 
 // ======= Begin Task 4 =======
+        taskCount++;
+        System.out.println("\n" + taskScreen + taskCount);
+
         var friend = 19; // Step 1
-        System.out.println("\n" + friend);
+        System.out.println(friend);
 
         friend += 2; // Step 2
         System.out.println(c1 + friend + c0 );
@@ -72,8 +84,11 @@ public class Main {
 // ======= End Task 4 =========
 
 // ======= Begin Task 5 =======
+        taskCount++;
+        System.out.println("\n" + taskScreen + taskCount);
+
         var frog = 3.5; // Step 1
-        System.out.println("\n" + frog);
+        System.out.println(frog);
 
         frog *= 10; // Step 2
         System.out.println(c1 + frog + c0 );
@@ -86,8 +101,11 @@ public class Main {
 // ======= End Task 5 =========
 
 // ======= Begin Task 6 =======
+        taskCount++;
+        System.out.println("\n" + taskScreen + taskCount);
+
         var firstBoxerWeight = 78.2;
-        System.out.println("\n" + "Вес первого боксера = " + c1 + firstBoxerWeight + c0 + " кг.");
+        System.out.println("Вес первого боксера = " + c1 + firstBoxerWeight + c0 + " кг.");
 
         var secondBoxerWeight = 82.7;
         System.out.println("Вес второго боксера = " + c1 + secondBoxerWeight + c0 + " кг.");
@@ -97,8 +115,11 @@ public class Main {
 // ======= End Task 6 =========
 
 // ======= Begin Task 7 =======
+        taskCount++;
+        System.out.println("\n" + taskScreen + taskCount);
+
         var boxerOneWeight = 78.2;
-        System.out.println("\n" + "Вес первого боксера = " + c1 + boxerOneWeight + c0 + " кг.");
+        System.out.println("Вес первого боксера = " + c1 + boxerOneWeight + c0 + " кг.");
 
         var boxerTwoWeight = 82.7;
         System.out.println("Вес второго боксера = " + c1 + boxerTwoWeight + c0 + " кг.");
@@ -111,20 +132,23 @@ public class Main {
 // ======= End Task 7 =========
 
 // ======= Begin Task 8 =======
+        taskCount++;
+        System.out.println("\n" + taskScreen + taskCount + ".1");
+
         var totalManHour = 640;
         var oneEmployeeNorm = 8;
         var totalEmployee = totalManHour/oneEmployeeNorm;
 
-        System.out.println("\n"+"Всего работников в компании — " + c1 + totalEmployee + c0 + " человек.");
+        System.out.println("Всего работников в компании — " + c1 + totalEmployee + c0 + " человек.");
+
+        System.out.println("\n" + taskScreen + taskCount + ".1");
 
         totalEmployee += 94;
         var manHour = totalManHour/totalEmployee;
         var s1 = "Если в компании работает ";
         var s2 = " человек, то всего ";
         var s3 = " часа работы может быть поделено между сотрудниками.";
-        System.out.println("\n"+ s1 + c1 + totalEmployee + c0 + s2 + c1 + manHour + c0 + s3);
-
-
+        System.out.println(s1 + c1 + totalEmployee + c0 + s2 + c1 + manHour + c0 + s3);
 // ======= End Task 8 =========
     }
 }
